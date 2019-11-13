45343cf47bd48958cff44c359226039bbc6afc23
Jim Safley <jimsafley@gmail.com>
2019-11-13 16:41:53 -0500

    Add land office data to map features

--- generate_feature_collections.py
+++ generate_feature_collections.py
@@ -21,15 +21,25 @@ def getFeatureCollection(type, year):
     states = {}
     for row in c.execute(sql, (type, year)):
         if row['state'] not in states:
-            states[row['state']] = {'coordinates': json.loads(row['coordinates'])}
+            states[row['state']] = {
+                'coordinates': json.loads(row['coordinates']),
+                'land_offices': {}
+            }
+        if row['land_office'] not in states[row['state']]['land_offices']:
+            states[row['state']]['land_offices'][row['land_office']] = {
+                'total_number': 0,
+                'total_acres': 0,
+            }
         if 'total_number' not in states[row['state']]:
             states[row['state']]['total_number'] = 0
         if 'total_acres' not in states[row['state']]:
             states[row['state']]['total_acres'] = 0
         if isinstance(row['number'], int):
             states[row['state']]['total_number'] += row['number']
+            states[row['state']]['land_offices'][row['land_office']]['total_number'] += row['number']
         if isinstance(row['acres'], (int, float)):
             states[row['state']]['total_acres'] += row['acres']
+            states[row['state']]['land_offices'][row['land_office']]['total_acres'] += row['acres']
 
     features = []
     for state, data in states.items():
@@ -40,6 +50,7 @@ def getFeatureCollection(type, year):
                 'state': state,
                 'total_acres': data['total_acres'],
                 'total_number': data['total_number'],
+                'land_offices': data['land_offices']
             },
             'geometry': {
                 'type': 'Polygon' if (1 == len(data['coordinates'])) else 'MultiPolygon',
